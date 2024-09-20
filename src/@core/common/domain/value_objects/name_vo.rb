ab25b7968e6236cf16db1e4d5cdb2cc893d9a15f
silva4dev <web4dev.lucas@outlook.com>
2024-09-20 08:22:47 -0300

    feat: :sparkles: add value object to name

---
+++ src/@core/common/domain/value_objects/name_vo.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../value_object'
+
+module Common
+  module Domain
+    module ValueObjects
+      class Name < ValueObject
+      end
+    end
+  end
+end
