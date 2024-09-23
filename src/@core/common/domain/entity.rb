d8bfed4c8826f53cc3f98f0d5fd7bda082fb596f
silva4dev <web4dev.lucas@outlook.com>
2024-09-23 20:19:10 -0300

    feat: :sparkles: add comments to entity base

--- src/@core/common/domain/entity.rb
+++ src/@core/common/domain/entity.rb
@@ -3,6 +3,9 @@
 module Common
   module Domain
     class Entity
+      # Entities are domain objects that represent important business concepts. They are responsible for encapsulating
+      # the state and behavior related to these concepts, and are fundamental for domain modeling.
+
       def to_hash
         raise NotImplementedError
       end
