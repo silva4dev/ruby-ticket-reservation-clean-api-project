d1d63c0542a3246905da8730023300d5db87a492
silva4dev <web4dev.lucas@outlook.com>
2024-09-23 21:33:19 -0300

    feat: :sparkles: add value object to uuid

---
+++ src/@core/common/domain/value_objects/uuid_vo.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'uuid'
+
+require_relative '../value_object'
+require_relative '../errors/invalid_uuid_error'
+
+module Common
+  module Domain
+    module ValueObjects
+      class Uuid < ValueObject
+        UUID_REGEX = /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i
+
+        def initialize(value = UUID.new.generate)
+          super(value)
+          validate
+        end
+
+        private
+
+        def validate
+          unless valid_uuid?(value)
+            raise Errors::InvalidUuidError.new(value)
+          end
+        end
+
+        def valid_uuid?(uuid)
+          uuid.match?(UUID_REGEX)
+        end
+      end
+    end
+  end
+end
