76f62fe8d646bc837b0e0d9ec319e1e82bda8c60
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 20:57:08 -0300

    test: :test_tube: remove asserts unnecessaries

--- tests/@core/events/domain/entities/partner_entity.test.rb
+++ tests/@core/events/domain/entities/partner_entity.test.rb
@@ -5,9 +5,7 @@ require_relative '../../../../../src/@core/events/domain/entities/partner_entity
 
 class PartnerTest < Minitest::Test
   def setup
-    @sut = Events::Domain::Entities::Partner.create({
-      name: 'John Doe'
-    })
+    @sut = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
   end
 
   def test_create_new_partner
@@ -15,16 +13,5 @@ class PartnerTest < Minitest::Test
     refute_nil @sut.id
     assert_instance_of Common::Domain::ValueObjects::Uuid, @sut.id
     assert_equal @sut.name, 'John Doe'
-    @sut = Events::Domain::Entities::Partner.new(
-      id: '84df48d0-5c35-013d-d217-00155d5361ff',
-      name: 'Mary Doe'
-    )
-    assert_equal @sut.id.value, '84df48d0-5c35-013d-d217-00155d5361ff'
-    assert_equal @sut.name, 'Mary Doe'
-    @other = Events::Domain::Entities::Partner.new(
-      id: @sut.id.value,
-      name: 'John Doe'
-    )
-    assert_equal @sut.equals(@other), true
   end
 end
