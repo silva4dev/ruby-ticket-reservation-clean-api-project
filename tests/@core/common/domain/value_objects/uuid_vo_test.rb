9b75237119e8e0522488db85d489e012501d7756
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 20:34:58 -0300

    test: :test_tube: add value object to uuid

---
+++ tests/@core/common/domain/value_objects/uuid_vo_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../../../../test_helper'
+require_relative '../../../../../src/@core/common/domain/value_objects/uuid_vo'
+
+class UuidTest < Minitest::Test
+  def setup
+    @sut = Common::Domain::ValueObjects::Uuid.new('84df48d0-5c35-013d-d217-00155d5361ff')
+  end
+
+  def test_create_new_uuid_vo
+    assert_equal @sut.value, '84df48d0-5c35-013d-d217-00155d5361ff'
+  end
+end
