f3afa50cae8d2561b63a02cb16385c80653d01da
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 08:02:19 -0300

    feat: :sparkles: add event spot entity

---
+++ src/@core/events/domain/entities/event_spot_entity.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative '../../../common/domain/aggregate_root'
+require_relative '../../../common/domain/value_objects/uuid_vo'
+
+module Events
+  module Domain
+    module Entities
+      class EventSpot < Common::Domain::AggregateRoot
+        attr_accessor :location, :is_reserved, :is_published
+
+        def initialize(id: nil, location:, is_reserved:, is_published:)
+          super()
+          @id = id.is_a?(String) ? Common::Domain::ValueObjects::Uuid.new(id) : id || Common::Domain::ValueObjects::Uuid.new
+          @location = location || nil
+          @is_reserved = is_reserved
+          @is_published = is_published
+        end
+
+        def self.create
+          new(
+            location: nil,
+            is_reserved: false,
+            is_published: false
+          )
+        end
+
+        # Convert EventSpot to hash
+        def to_hash
+          {
+            id: @id.value,
+            location: @location,
+            is_reserved: @is_reserved,
+            is_published: @is_published
+          }
+        end
+      end
+    end
+  end
+end
