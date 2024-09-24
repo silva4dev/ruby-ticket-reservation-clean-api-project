7ef91287c1e084c52d18ba0d28324d2dca51ec91
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 20:56:49 -0300

    test: :test_tube: add event spot entity

---
+++ tests/@core/events/domain/entities/event_spot_entity_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../../../../test_helper'
+require_relative '../../../../../src/@core/events/domain/entities/event_spot_entity'
+
+class EventSpotTest < Minitest::Test
+  def setup
+    @sut = Events::Domain::Entities::EventSpot.create
+  end
+
+  def test_create_new_event_spot
+    assert_instance_of Events::Domain::Entities::EventSpot, @sut
+    refute_nil @sut.id
+    assert_nil @sut.location
+    assert_equal false, @sut.is_reserved
+    assert_equal false, @sut.is_published
+  end
+end
