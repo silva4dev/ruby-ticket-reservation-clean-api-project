094e251704700c7676dadd4fa89ff3eae88ac89b
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 08:01:57 -0300

    feat: :sparkles: add event entity

---
+++ src/@core/events/domain/entities/event_entity.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative '../../../common/domain/aggregate_root'
+require_relative '../../../common/domain/value_objects/uuid_vo'
+
+module Events
+  module Domain
+    module Entities
+      class Event < Common::Domain::AggregateRoot
+        attr_accessor :name, :description, :date, :is_published, :total_spots, :total_spots_reserved, :partner_id
+
+        def initialize(id: nil, name:, description:, date:, is_published:, total_spots:, total_spots_reserved:, partner_id:)
+          super()
+          @id = id.is_a?(String) ? Common::Domain::ValueObjects::Uuid.new(id) : id || Common::Domain::ValueObjects::Uuid.new
+          @name = name
+          @description = description || nil
+          @date = date
+          @is_published = is_published
+          @total_spots = total_spots
+          @total_spots_reserved = total_spots_reserved
+          @partner_id = partner_id.is_a?(String) ? Common::Domain::ValueObjects::Uuid.new(partner_id) : partner_id
+        end
+
+        def self.create(command)
+          new(
+            name: command[:name],
+            description: command[:description] || nil,
+            date: command[:date],
+            is_published: false,
+            total_spots: 0,
+            total_spots_reserved: 0,
+            partner_id: Common::Domain::ValueObjects::Uuid.new(command[:partner_id])
+          )
+        end
+
+        def to_hash
+          {
+            id: @id.value,
+            name: @name,
+            description: @description,
+            date: @date,
+            is_published: @is_published,
+            total_spots: @total_spots,
+            total_spots_reserved: @total_spots_reserved,
+            partner_id: @partner_id.value,
+          }
+        end
+      end
+    end
+  end
+end
