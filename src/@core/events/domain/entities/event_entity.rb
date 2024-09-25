bf25a11e6437deb610ca7a57ce95be2c40294fd1
silva4dev <web4dev.lucas@outlook.com>
2024-09-24 22:27:58 -0300

    feat: :sparkles: add sections field to event entity

--- src/@core/events/domain/entities/event_entity.rb
+++ src/@core/events/domain/entities/event_entity.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'set'
+
 require_relative '../../../common/domain/aggregate_root'
 require_relative '../../../common/domain/value_objects/uuid_vo'
 
@@ -7,9 +9,9 @@ module Events
   module Domain
     module Entities
       class Event < Common::Domain::AggregateRoot
-        attr_accessor :name, :description, :date, :is_published, :total_spots, :total_spots_reserved, :partner_id
+        attr_accessor :name, :description, :date, :is_published, :total_spots, :total_spots_reserved, :partner_id, :sections
 
-        def initialize(id: nil, name:, description:, date:, is_published:, total_spots:, total_spots_reserved:, partner_id:)
+        def initialize(id: nil, name:, description:, date:, is_published:, total_spots:, total_spots_reserved:, partner_id:, sections: Set.new)
           super()
           @id = id.is_a?(String) ? Common::Domain::ValueObjects::Uuid.new(id) : id || Common::Domain::ValueObjects::Uuid.new
           @name = name
@@ -19,6 +21,7 @@ module Events
           @total_spots = total_spots
           @total_spots_reserved = total_spots_reserved
           @partner_id = partner_id.is_a?(String) ? Common::Domain::ValueObjects::Uuid.new(partner_id) : partner_id
+          @sections = sections || Set.new
         end
 
         def self.create(command)
@@ -29,7 +32,8 @@ module Events
             is_published: false,
             total_spots: 0,
             total_spots_reserved: 0,
-            partner_id: Common::Domain::ValueObjects::Uuid.new(command[:partner_id])
+            partner_id: Common::Domain::ValueObjects::Uuid.new(command[:partner_id]),
+            sections: command[:sections] || Set.new
           )
         end
 
@@ -43,6 +47,7 @@ module Events
             total_spots: @total_spots,
             total_spots_reserved: @total_spots_reserved,
             partner_id: @partner_id.value,
+            sections: @sections.map(&:to_hash)
           }
         end
       end
