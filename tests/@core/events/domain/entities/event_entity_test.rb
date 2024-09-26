19f36b5d820773be1b777fa26d00b8ee267fa5f9
silva4dev <web4dev.lucas@outlook.com>
2024-09-26 08:05:59 -0300

    test: :test_tube: change event entity

--- tests/@core/events/domain/entities/event_entity_test.rb
+++ tests/@core/events/domain/entities/event_entity_test.rb
@@ -25,8 +25,7 @@ class EventTest < Minitest::Test
 
     @spot = Events::Domain::Entities::EventSpot.create()
 
-    @sut.sections.add(@section)
-    @section.spots.add(@spot)
+    @sut.add_section(@section)
   end
 
   def test_create_new_event
@@ -35,8 +34,9 @@ class EventTest < Minitest::Test
     assert_equal 'Test Event', @sut.name
     assert_equal 'A description of the test event', @sut.description
     assert_instance_of DateTime, @sut.date
+    assert_equal @sut.sections.count, 1
     assert_equal false, @sut.is_published
-    assert_equal 0, @sut.total_spots
+    assert_equal 100, @sut.total_spots
     assert_equal 0, @sut.total_spots_reserved
     assert_equal '123e4567-e89b-12d3-a456-426614174000', @sut.partner_id.value
   end
