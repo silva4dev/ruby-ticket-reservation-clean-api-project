c6842d253512ed4d10c66a2cda0d00abc600c124
silva4dev <web4dev.lucas@outlook.com>
2024-09-26 22:19:11 -0300

    test: :test_tube: add event after partner created

--- tests/@core/events/domain/entities/partner_entity_test.rb
+++ tests/@core/events/domain/entities/partner_entity_test.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
+require 'date'
+
 require_relative '../../../../test_helper'
 require_relative '../../../../../src/@core/events/domain/entities/partner_entity'
+require_relative '../../../../../src/@core/events/domain/entities/event_entity'
 
 class PartnerTest < Minitest::Test
   def setup
-    @sut = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
+    @sut = Events::Domain::Entities::Partner.create({
+      name: 'John Doe'
+    })
+
+    @event = @sut.initEvent({
+      name: 'Test Event',
+      description: 'A description of the test event',
+      date: DateTime.now
+    })
   end
 
   def test_create_new_partner
@@ -13,5 +24,13 @@ class PartnerTest < Minitest::Test
     refute_nil @sut.id
     assert_instance_of Common::Domain::ValueObjects::Uuid, @sut.id
     assert_equal @sut.name, 'John Doe'
+  end
+
+  def test_create_new_event
+    assert_instance_of Events::Domain::Entities::Event, @event
+    refute_nil @event.id
+    assert_instance_of Common::Domain::ValueObjects::Uuid, @event.id
+    assert_equal 'Test Event', @event.name
+    assert_equal 'A description of the test event', @event.description
   end
 end
