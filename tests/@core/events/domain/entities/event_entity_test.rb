d5c2105c636d6d7674dac666816356e47530baf1
silva4dev <web4dev.lucas@outlook.com>
2024-09-28 22:36:26 -0300

    test: :test_tube: add publish all event items

--- tests/@core/events/domain/entities/event_entity_test.rb
+++ tests/@core/events/domain/entities/event_entity_test.rb
@@ -10,19 +10,19 @@ require_relative '../../../../../src/@core/common/domain/value_objects/uuid_vo'
 
 class EventTest < Minitest::Test
   def setup
-    @sut = Events::Domain::Entities::Event.create({
+    @sut = Events::Domain::Entities::Event.create(
       name: 'Test Event',
       description: 'A description of the test event',
       date: DateTime.now,
-      partner_id: Common::Domain::ValueObjects::Uuid.new,
-    })
+      partner_id: Common::Domain::ValueObjects::Uuid.new
+    )
 
-    @sut.add_section({
+    @sut.add_section(
       name: 'VIP Section',
       description: 'Exclusive access to the VIP area',
       total_spots: 100,
       price: 150.50
-    })
+    )
   end
 
   def test_create_new_event
@@ -31,10 +31,30 @@ class EventTest < Minitest::Test
     assert_equal 'Test Event', @sut.name
     assert_equal 'A description of the test event', @sut.description
     assert_instance_of DateTime, @sut.date
-    assert_equal @sut.sections.count, 1
+    assert_equal 1, @sut.sections.count
     assert_equal false, @sut.is_published
     assert_equal 100, @sut.total_spots
     assert_equal 0, @sut.total_spots_reserved
     assert_equal 100, @sut.sections.first.spots.size
+  end
+
+  def test_publish_all_event_items
+    @sut.add_section(
+      name: 'VIP Section 2',
+      description: 'Exclusive access to the VIP area 2',
+      total_spots: 25,
+      price: 75.25
+    )
+
+    @sut.publish_all
+
+    assert_equal true, @sut.is_published
+    assert_equal true, @sut.sections.to_a.first.is_published
+    assert_equal true, @sut.sections.to_a.last.is_published
+    @sut.sections.each do |section|
+      section.spots.each do |spot|
+        assert_equal true, spot.is_published
+      end
+    end
   end
 end
