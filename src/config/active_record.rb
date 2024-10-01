a6f638f9adccd8456698c2fde1f4b7ef2c391935
silva4dev <web4dev.lucas@outlook.com>
2024-10-01 20:24:06 -0300

    feat: :sparkles: add establish connection to active record

---
+++ src/config/active_record.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'active_record'
+require 'mysql2'
+require 'yaml'
+require 'logger'
+
+config_path = File.join(__dir__, "database.yml")
+ActiveRecord::Base.configurations = YAML.load_file(config_path)
+ActiveRecord::Base.establish_connection(:development)
+
+logger = Logger.new(STDOUT)
+logger.formatter = proc do |severity, datetime, progname, msg|
+   "#{msg}\n"
+end
+ActiveRecord::Base.logger = logger
