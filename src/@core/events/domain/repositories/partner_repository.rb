4ba58a574767d219ac95e29795bd79521c8b8266
silva4dev <web4dev.lucas@outlook.com>
2024-10-02 22:49:21 -0300

    feat: :sparkles: add partner repository

---
+++ src/@core/events/domain/repositories/partner_repository.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../../../common/domain/repository'
+
+module Events
+  module Domain
+    module Repositories
+      class PartnerRepository < Common::Domain::Repository; end
+    end
+  end
+end
