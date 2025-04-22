b8da45a3615db12f1778bf2c475bf57710b8fd77
silva4dev <web4dev.lucas@outlook.com>
2025-04-21 21:22:54 -0300

    test: 🧪 simplify code

--- tests/@core/events/infra/models/partner_model_test.rb
+++ tests/@core/events/infra/models/partner_model_test.rb
@@ -7,11 +7,6 @@ require_relative '../../../../../src/@core/events/domain/entities/partner_entity
 class PartnerModelTest < Minitest::Test
   def setup
     DatabaseCleaner.start
-    @partner = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
-    @sut = Events::Infra::Models::Partner.create({
-      id: @partner.id.value,
-      name: @partner.name
-    })
   end
 
   def teardown
@@ -19,6 +14,12 @@ class PartnerModelTest < Minitest::Test
   end
 
   def test_create_new_partner
+    @partner = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
+    @sut = Events::Infra::Models::Partner.create({
+      id: @partner.id.value,
+      name: @partner.name
+    })
+
     assert_equal @sut.id, @partner.id.value
     assert_equal @sut.name, @partner.name
   end
--- tests/@core/events/infra/repositories/partner_mysql_repository_test.rb
+++ tests/@core/events/infra/repositories/partner_mysql_repository_test.rb
@@ -8,12 +8,6 @@ require_relative '../../../../../src/@core/events/domain/entities/partner_entity
 class PartnerMysqlRepositoryTest < Minitest::Test
   def setup
     DatabaseCleaner.start
-    @partner = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
-    @partner_repository = Events::Infra::Repositories::PartnerMysqlRepository.new(
-      Events::Infra::Models::Partner
-    )
-    @partner_repository.add(@partner)
-    @sut = @partner_repository.find_by_id(@partner.id.value)
   end
 
   def teardown
@@ -21,6 +15,15 @@ class PartnerMysqlRepositoryTest < Minitest::Test
   end
 
   def test_create_new_partner
+    @partner = Events::Domain::Entities::Partner.create({ name: 'John Doe' })
+
+    @partner_repository = Events::Infra::Repositories::PartnerMysqlRepository.new(
+      Events::Infra::Models::Partner
+    )
+    @partner_repository.add(@partner)
+
+    @sut = @partner_repository.find_by_id(@partner.id.value)
+
     assert @sut.id.equals(@partner.id)
   end
 end
