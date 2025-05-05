33c21bba0f2e09e506de14f854ea69b85a4c2671
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2025-05-05 02:00:38 -0300

    chore: ✨ use env

--- src/database/connection.rb
+++ src/database/connection.rb
@@ -4,8 +4,15 @@ require 'active_record'
 require 'mysql2'
 require 'yaml'
 require 'logger'
+require 'dotenv'
 
-environment = ENV['ENVIRONMENT'].nil? ? :development : ENV['ENVIRONMENT'].to_sym
+if ENV['RACK_ENV'] == 'test'
+  Dotenv.load!('.env.test')
+else
+  Dotenv.load!('.env.development')
+end
+
+environment = ENV['RACK_ENV'].to_sym
 config_path = File.join(__dir__, 'database.yml')
 ActiveRecord::Base.configurations = YAML.load_file(config_path)
 ActiveRecord::Base.establish_connection(environment)
